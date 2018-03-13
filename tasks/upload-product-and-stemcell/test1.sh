c134871b40f1a42a4405bc5620c52fdbd840594d
krishnaspk <37294489+krishnaspk@users.noreply.github.com>
2018-03-13 11:15:31 +0000

    cf v

--- tasks/upload-product-and-stemcell/test1.sh
+++ tasks/upload-product-and-stemcell/test1.sh
@@ -6,6 +6,7 @@
 #export CF_New_Quota=test-samp-org-quota
 #export CF_New_Space_Quota=test-samp-space-quota
 export CFspace=$CF_SPACE
+cf --version
 #export CF_New_USER_PASS=`openssl rand -base64 15`
 echo "$CF_New_Org , $CF_New_Space , $CF_New_User , $CF_New_Quota , $CF_New_Space_Quota , $CF_ORG"
 cf login -a "${CF_API}" -u "${CF_USERNAME}" -p "${CF_PASSWORD}" -o "${CF_ORG}" -s Demo
