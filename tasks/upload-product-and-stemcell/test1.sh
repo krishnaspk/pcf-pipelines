3431125973b53075e60f317a5840a7d5e3603010
krishnaspk <37294489+krishnaspk@users.noreply.github.com>
2018-03-14 08:50:02 +0000

    cf chng

--- tasks/upload-product-and-stemcell/test1.sh
+++ tasks/upload-product-and-stemcell/test1.sh
@@ -9,8 +9,8 @@ export CFspace=$CF_SPACE
 cf help create-user
 #export CF_New_USER_PASS=`openssl rand -base64 15`
 echo "$CF_New_Org , $CF_New_Space , $CF_New_User , $CF_New_Quota , $CF_New_Space_Quota , $CF_ORG"
-cf login -a "${CF_API}" -u "${CF_USERNAME}" -p "${CF_PASSWORD}" -o "${CF_ORG}" -s Demo
+cf login -a "${CF_API}" -u "${CF_USERNAME}" -p "${CF_PASSWORD}" -o "${CF_ORG}" -s system
 cf help -a 
-cf target -s Demo
+cf target -s system
 echo "$CFspace"
 echo -e "login to PCF Eco system successful using $CF_USERNAME\n\n"
